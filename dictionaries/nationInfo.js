0b6e67d840ec924c3f172a8bec5fc2db4d6b6345
itsdragonic <fzawislak25@lrchs.org>
2025-08-12 14:45:33 -0500

    0.0.1
    + map logic

---
+++ dictionaries/nationInfo.js
@@ -0,0 +1,22 @@
+const nationInfo = {
+    "rome": {
+        "name": "Rome",
+        "empire_name": "Roman Empire",
+        "color": "#af0404",
+    },
+    "thrace": {
+        "name": "Thrace",
+        "empire_name": "Thracian Empire",
+        "color": "#636473",
+    },
+    "parthia": {
+        "name": "Partia",
+        "empire_name": "Partian Empire",
+        "color": "#eaa329",
+    },
+    "armenia": {
+        "name": "Armenia",
+        "empire_name": "Armenian Empire",
+        "color": "#ae5a3d",
+    },
+}
---
+++ drawmap.js
@@ -0,0 +1,4 @@
+function drawMap(ctx) {
+    ctx.drawImage(map_empty, 0, 0);
+    ctx.drawImage(map_provinces, 0, 0);
+}
